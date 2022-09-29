6220c6a841cb39789e47b7aeeec52010da69889d
Vincent van Duijn <vincent.van.duijn@devoteam.com>
2022-09-29 10:59:19 +0200

    Some files to prepare the project before starting

---
+++ src/main/java/com/devoteam/valhalla/ValhallaApplication.java
@@ -0,0 +1,13 @@
+package com.devoteam.valhalla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValhallaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValhallaApplication.class, args);
+    }
+
+}
