eefc91b49b0b1df5d37310eb44d5eca431033911
Vincent van Duijn <vincent.van.duijn@devoteam.com>
2022-10-16 21:39:41 +0200

    Changes structure vv-10

--- spring-cloud-gateway/src/main/java/com/valhallavincent/springcloudgateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/valhallavincent/springcloudgateway/SpringCloudGatewayApplication.java
@@ -3,12 +3,14 @@ package com.valhallavincent.springcloudgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@EnableEurekaClient
 public class SpringCloudGatewayApplication {
 
 	public static void main(String[] args) {
